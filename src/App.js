d9559946e90b8966b9a6db39aacf8d2c24fa76ae
Shriram <shriram@kinshipsystems.com>
2020-01-29 20:43:14 +0530

    ui updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App container">
-      <div className="row flex-column align-items-center">
+      <div className="flex-column align-items-center">
         <h1>Lets Generate a Fibonacci Series</h1>
         <Series />
       </div>
